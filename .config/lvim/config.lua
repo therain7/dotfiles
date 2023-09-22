0338e4b81eeb0fd96df331411a6d92ec8a5a4b50
Andrei <therain.i@yahoo.com>
2023-09-23 01:10:31 +0300

    use spaces instead of tabs in lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -5,7 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.diagnostic.config({
-	virtual_text = false,
+    virtual_text = false,
 })
 
 lvim.format_on_save.enabled = true
@@ -15,29 +15,29 @@ lvim.format_on_save.enabled = true
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.builtin.which_key.mappings["W"] = {
-	"<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)"
+    "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)"
 }
 
 lvim.builtin.which_key.mappings["o"] = {
-	"<cmd>SymbolsOutline<cr>", "Symbols outline"
+    "<cmd>SymbolsOutline<cr>", "Symbols outline"
 }
 
 -- change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-	-- for input mode
-	i = {
-		["<C-j>"] = actions.move_selection_next,
-		["<C-k>"] = actions.move_selection_previous,
-		["<C-n>"] = actions.cycle_history_next,
-		["<C-p>"] = actions.cycle_history_prev,
-	},
-	-- for normal mode
-	n = {
-		["<C-j>"] = actions.move_selection_next,
-		["<C-k>"] = actions.move_selection_previous,
-	},
+    -- for input mode
+    i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+    },
+    -- for normal mode
+    n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+    },
 }
 
 
@@ -48,66 +48,66 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "kotlin_lang
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "texlab" })
 
 lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-	return server ~= "emmet_ls"
+    return server ~= "emmet_ls"
 end, lvim.lsp.automatic_configuration.skipped_servers)
 
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-	{
-		name = "black",
-		args = { "--line-length", "95" }
-	},
-	{
-		name = "isort",
-		args = { "--profile", "black" }
-	},
-	{
-		name = "prettier",
-		args = { "--tab-width", "4" }
-	},
+    {
+        name = "black",
+        args = { "--line-length", "95" }
+    },
+    {
+        name = "isort",
+        args = { "--profile", "black" }
+    },
+    {
+        name = "prettier",
+        args = { "--tab-width", "4" }
+    },
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-	{
-		name = "flake8",
-		args = { "--max-line-length", "95" }
-	},
-	{
-		name = "mypy",
-		args = { "--strict" }
-	}
+    {
+        name = "flake8",
+        args = { "--max-line-length", "95" }
+    },
+    {
+        name = "mypy",
+        args = { "--strict" }
+    }
 }
 
 
 -- additional Plugins
 lvim.plugins = {
-	-- improve the default vim.ui interfaces
-	{ "stevearc/dressing.nvim" },
+    -- improve the default vim.ui interfaces
+    { "stevearc/dressing.nvim" },
 
-	-- tree like view for symbols in neovim using the lsp
-	{
-		"simrat39/symbols-outline.nvim",
-		config = function()
-			require("symbols-outline").setup()
-		end
-	},
+    -- tree like view for symbols in neovim using the lsp
+    {
+        "simrat39/symbols-outline.nvim",
+        config = function()
+            require("symbols-outline").setup()
+        end
+    },
 
-	-- add/change/delete surrounding delimiter pairs with ease
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	},
+    -- add/change/delete surrounding delimiter pairs with ease
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
 
-	-- colorschemes
-	{ "catppuccin/nvim",       name = "catppuccin" }
+    -- colorschemes
+    { "catppuccin/nvim",       name = "catppuccin" }
 }
 
 lvim.colorscheme = "catppuccin"
