80caf8107ae4c3cca056956ad59f543c5f334835
Andrei <therain.i@yahoo.com>
2025-05-19 03:59:22 +0300

    vifari: `u` to open previously closed tab

--- mac/.hammerspoon/init.lua
+++ mac/.hammerspoon/init.lua
@@ -3,6 +3,7 @@ hs.loadSpoon("Vifari")
 spoon.Vifari:start({
     scrollStep = 200,
     mapping = {
+        ["u"] = { { "cmd", "shift" }, "t" },
         ["H"] = { { "cmd", "shift" }, "[" },
         ["L"] = { { "cmd", "shift" }, "]" },
         ["J"] = "cmdScrollHalfPageDown",
