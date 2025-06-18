77ef25cbc51818347b7daece333d65cfa7888346
Andrei <therain.i@yahoo.com>
2025-06-18 21:44:21 +0300

    sway: copy-paste stuff

--- linux/.config/sway/copy-paste.sh
+++ linux/.config/sway/copy-paste.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+case $1 in
+  "copy") key=c ;;
+  "paste") key=v ;;
+  *) exit 1 ;;
+esac
+
 app=$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id')
 
 if [[ $app == "Alacritty" ]]; then
-   wtype -M ctrl -M shift -P c
+   wtype -M ctrl -M shift -P $key
 else
-   wtype -M ctrl -P c
+   wtype -M ctrl -P $key
 fi
