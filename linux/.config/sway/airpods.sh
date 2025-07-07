250ce42e2045aca5d850109a896b2d088e1087c3
Andrei <therain.i@yahoo.com>
2025-07-07 22:01:22 +0300

    airpods.sh: nice notifications

--- linux/.config/sway/airpods.sh
+++ linux/.config/sway/airpods.sh
@@ -4,8 +4,10 @@ airpods=$(bluetoothctl devices | grep -i airpods | cut -d' ' -f2)
 connected=$(bluetoothctl devices Connected | grep $airpods)
 
 if [[ -z $connected ]]; then
+    notify-send "connecting airpods"
     bluetoothctl connect $airpods
 else
     # already connected, disconnect
+    notify-send "disconnecting airpods"
     bluetoothctl disconnect $airpods
 fi
