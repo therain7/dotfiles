13f32febb727922581e8ff69d1434146de7a3c0f
Andrei <therain.i@yahoo.com>
2026-02-06 19:57:35 +0300

    mac: add `defaults.sh`

---
+++ mac/defaults.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# show & hide dock faster
+defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.dock autohide-time-modifier -float 0.2
+
+# repeat keys on hold
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# disable annoying inline text popup (caps lock / kb layout indicator)
+sudo defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool false
