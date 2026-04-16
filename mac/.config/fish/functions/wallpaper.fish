1dbbf5ccf9695c1699907befe8fb4425d4ff09e4
Andrei <therain.i@yahoo.com>
2026-04-16 20:42:03 +0300

    mac: use `osascript` in `wallpaper.fish`

--- mac/.config/fish/functions/wallpaper.fish
+++ mac/.config/fish/functions/wallpaper.fish
@@ -1,8 +1,4 @@
 function wallpaper
     set path (realpath $argv[1])
-
-    /usr/libexec/PlistBuddy -c "set AllSpacesAndDisplays:Desktop:Content:Choices:0:Files:0:relative file:///$path" \
-        ~/Library/Application\ Support/com.apple.wallpaper/Store/Index.plist
-
-    killall WallpaperAgent
+    osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"$path\""
 end
